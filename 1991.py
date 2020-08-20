66940eeddfba1943ce77ba9891a189f62dbd5b6f
Lee Jun Hyuk <ljhjoon00@gmail.com>
2020-08-20 15:40:34 +0900

    #commit 9 : Modified codes
    code # : 1991

--- 1991.py
+++ 1991.py
@@ -4,32 +4,72 @@
 import sys
 
 class Node:
-    def __init__(self, content, left, right):
-        self.left = left
-        self.right = right
-        self.content = content
+    def __init__(self, item, left, right, parent):
+        self.item = item
+        self.left = None
+        self.right = None
+        self.parent = parent
+
+        if left != ".":
+            self.left = Node(left, ".", ".", self)
+
+        if right != ".":
+            self.right = Node(right, ".", ".", self)
 
     def is_left(self):
-        if self.left == ".":
+        if self.left is None:
             return False
         return True
 
     def is_right(self):
-        if self.right == ".":
+        if self.right is None:
             return False
         return True
 
 class Tree:
     def __init__(self):
-        self.nodes = {}
+        self.Nnode = 0
+        self.root = None
 
-    def input_nodes(self, content, left, right):
-        if content not in self.nodes:
-            self.nodes[content] = Node(content, left, right)
+    def input_nodes(self, item, left, right):
+        if self.Nnode == 0:
+            self.root = Node(item, left, right, None)
 
-    def preorder(self, root_content):
-        node = self.nodes[root_content]
-        print(root_content, end = '')
+        else:
+            node = self.traverse(self.root, item)
+            if node is None:
+                raise NameError
+
+            if node.parent.is_left() and node.parent.left.item == item:
+                node.parent.left = Node(item, left, right, node.parent)
+
+            else:
+                node.parent.right = Node(item, left, right, node.parent)
+
+
+        self.Nnode += 1
+
+
+    
+    def traverse(self, node, item):
+        if node.item == item:
+            return node
+
+        if node.is_left():
+            result_left = self.traverse(node.left, item)
+            if result_left is not None:
+                return result_left
+
+        if node.is_right():
+            result_right = self.traverse(node.right, item)
+            if result_right is not None:
+                return result_right
+
+        return None
+
+
+    def preorder(self, node):
+        print(node.item, end = '')
 
         if node.is_left():
             self.preorder(node.left)
@@ -37,19 +77,17 @@ class Tree:
         if node.is_right():
             self.preorder(node.right)
 
-    def inorder(self, root_content):
-        node = self.nodes[root_content]
+    def inorder(self, node):
 
         if node.is_left():
             self.inorder(node.left)
 
-        print(root_content, end = '')
+        print(node.item, end = '')
 
         if node.is_right():
             self.inorder(node.right)
 
-    def postorder(self, root_content):
-        node = self.nodes[root_content]
+    def postorder(self, node):
 
         if node.is_left():
             self.postorder(node.left)
@@ -57,7 +95,7 @@ class Tree:
         if node.is_right():
             self.postorder(node.right)
 
-        print(root_content, end= '')
+        print(node.item, end= '')
 
 
 if __name__ == "__main__":
@@ -65,12 +103,12 @@ if __name__ == "__main__":
     tree = Tree()
 
     for i in range(Nnode):
-        content, left, right = tuple(sys.stdin.readline().split())
-        tree.input_nodes(content, left, right)
+        item, left, right = tuple(sys.stdin.readline().split())
+        tree.input_nodes(item, left, right)
 
-    tree.preorder('A')
+    tree.preorder(tree.root)
     print()
-    tree.inorder('A')
+    tree.inorder(tree.root)
     print()
-    tree.postorder('A')
+    tree.postorder(tree.root)
     print()
