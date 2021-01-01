689701496cb71b75fce11fc71084af9a5be183da
Lee Jun Hyuk <ljhjoon00@gmail.com>
2021-01-01 13:39:24 +0900

    #commit 36 - 2 : Code Numbering error
    - Deleted code # : 1271_error.py
    - Modified code # : 1550.py

--- Python3/1550.py
+++ Python3/1550.py
@@ -1,12 +1,36 @@
-#https://www.acmicpc.net/problem/1550
+# https://www.acmicpc.net/problem/1550
 # 20200421
 
 import sys
 
-money, person = map(int, sys.stdin.readline().split())
+dict_16 = {
+    'A' : 10,
+    'B' : 11,
+    'C' : 12,
+    'D' : 13,
+    'E' : 14,
+    'F' : 15
+}
 
-each = money // person
-rest = money % person
+def Decimal_change(str_16):
+    conv_num = 0
+    base = 1
+    split = []
+    for i in str_16:
+        if i > '9':
+            i = dict_16[i]
+        else:
+            i = int(i)
+        split.append(i)
 
-print(each)
-print(rest)
+    split.reverse()
+    for digit in split:
+        conv_num += base * digit
+        base *= 16
+
+    return conv_num
+
+str_16 = sys.stdin.readline().strip()
+num_10 = Decimal_change(str_16)
+
+sys.stdout.write(str(num_10))
