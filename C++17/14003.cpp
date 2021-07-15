80b8838c319f96cc17a720c3832bea118a87394c
Lee Jun Hyuk <ljhjoon00@gmail.com>
2021-07-15 19:47:45 +0900

    #commit 46 : C++ code modification
    - Modified code # : 14003.cpp

--- C++17/14003.cpp
+++ C++17/14003.cpp
@@ -6,11 +6,12 @@
 #include <vector>
 
 #define MAXLEN 1000000
+#define LOWER_BOUND -1000000001
 
 int input[MAXLEN+1] = {0};
 
 /* The minumum end value of a subsequence whose length is its idx */
-int endval_per_length[MAXLEN+1] = {0};
+int endval_per_length[MAXLEN+1] = {LOWER_BOUND};
 
 /* Index of the endval_per_length */
 int endidx_per_length[MAXLEN+1] = {0};
@@ -48,10 +49,9 @@ int main(){
         /* cur length means the maximum legth of subseq that ends with current value */
         cur_length = binary_search_idx(endval_per_length, 0, max_length, input[idx]) + 1;
 
-        /* max length is maximum lengths among cur lengths */
+        /* Update the max length */
         if(cur_length > max_length){
             max_length = cur_length;
-        
         }
 
         /* update the end value as current value */
@@ -59,24 +59,24 @@ int main(){
         endidx_per_length[cur_length] = idx;
 
         prev_idx[idx] = endidx_per_length[cur_length - 1];
-        
     }
 
     printf("%d\n", max_length);
 
-    std::vector<int> LIS;
+    std::vector<int> result;
     int cur_idx = endidx_per_length[max_length];
 
+    /* Stack the value of the results at the vector */
     while(cur_idx != 0){
-        LIS.push_back(input[cur_idx]);
+        result.push_back(input[cur_idx]);
         cur_idx = prev_idx[cur_idx];
     }
 
+    /* Print the results in the right order */
     std::vector<int>::reverse_iterator rit;
-    for(rit = LIS.rbegin(); rit != LIS.rend(); rit++){
+    for(rit = result.rbegin(); rit != result.rend(); rit++){
         printf("%d ", *rit);
     }
-
 
     return 0;
 }
