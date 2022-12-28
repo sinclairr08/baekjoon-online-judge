1b858edcbf96a67c014a035a4113411b617ebec7
Sinclairr08 <ljhjoon00@gmail.com>
2022-12-28 23:50:37 +0900

    ‌init: Re-initialize a repository by adding codes written in June 2022

---
+++ format.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <vector>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    int arg1, arg2;
+
+    scanf("%d %d", &arg1, &arg2);
+
+    return 0;
+}
---
+++ format.py
@@ -0,0 +1,5 @@
+import sys
+
+if __name__ == "__main__":
+    arg1, arg2 = map(int, sys.stdin.readline().split())
+    arg = sys.stdin.readline().strip()
